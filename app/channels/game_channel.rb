7f7a0e907da162a74d618e052b17f34ff92dfc5e
timloughrist <tim.loughrist@gmail.com>
2023-03-18 11:38:55 -0500

    working actioncable

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -2,6 +2,9 @@ class GameChannel < ApplicationCable::Channel
 
   def subscribed
     stream_from "game#{params[:id]}"
+    game = Game.find(params[:id])
+    package = game.package
+    ActionCable.server.broadcast("game#{params[:id]}", package)
   end
 
 end
