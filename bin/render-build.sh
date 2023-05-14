48731b21951b1ae4c1336cae84de85654dd8cd5d
timloughrist <tim.loughrist@gmail.com>
2023-05-14 14:38:05 -0500

    add client

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,8 @@ set -o errexit
 bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
+
+npm install --prefix client && npm run build --prefix client
+
 bundle exec rake db:migrate
+
